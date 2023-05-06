08ddec10d6a0526f0fa3e4ec2d7a7c2c966239ae
DinhCuong <nguyendinhcuongyp@gmail.com>
2023-05-06 22:22:25 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Neue: ['Comic Neue', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
